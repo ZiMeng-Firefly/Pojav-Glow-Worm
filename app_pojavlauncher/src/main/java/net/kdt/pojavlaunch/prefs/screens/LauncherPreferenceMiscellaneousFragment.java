c7c2227747b795d89befa6ce72fac306e38d7985
Vera-Firefly <1964762970@qq.com>
2024-10-06 09:11:48 +0800

    测试

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceMiscellaneousFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceMiscellaneousFragment.java
@@ -3,6 +3,11 @@ package net.kdt.pojavlaunch.prefs.screens;
 import android.os.Bundle;
 
 import androidx.preference.Preference;
+import androidx.preference.SwitchPreference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import com.firefly.utils.PGWTools;
+import com.firefly.ui.dialog.CustomDialog;
 
 import net.kdt.pojavlaunch.R;
 import net.kdt.pojavlaunch.Tools;
@@ -15,5 +20,29 @@ public class LauncherPreferenceMiscellaneousFragment extends LauncherPreferenceF
         if (!Tools.checkVulkanSupport(driverPreference.getContext().getPackageManager())) {
             driverPreference.setVisible(false);
         }
+        SwitchPreference useSystemVulkan = requirePreference("zinkPreferSystemDriver", SwitchPreference.class);
+        useSystemVulkan.setOnPreferenceChangeListener((p, v) -> {
+            boolean isAdreno = PGWTools.isAdrenoGPU();
+            if (isAdreno) {
+                onCheckGPUDialog(p);
+                return false;
+            }
+            return true;
+        });
     }
+
+    private void onCheckGPUDialog(Preference pre) {
+        new CustomDialog.Builder(getContext())
+                .setTitle("No No No No No!")
+                .setMessage("text")
+                .setConfirmListener(R.string.preference_rendererexp_alertdialog_done, customView -> {
+                    ((SwitchPreference) pre).setChecked(true);
+                    return true;
+                })
+                .setCancelListener(R.string.preference_rendererexp_alertdialog_cancel, customView -> true)
+                .setCancelable(false)
+                .build()
+                .show();
+    }
+
 }
