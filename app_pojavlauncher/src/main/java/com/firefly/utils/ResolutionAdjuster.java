48a1298e007635cb25694664732021ef731fa91f
Vera-Firefly <1964762970@qq.com>
2024-10-09 11:06:24 +0800

    测试

--- app_pojavlauncher/src/main/java/com/firefly/utils/ResolutionAdjuster.java
+++ app_pojavlauncher/src/main/java/com/firefly/utils/ResolutionAdjuster.java
@@ -7,9 +7,15 @@ import android.widget.TextView;
 import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.TextureView;
+import android.view.SurfaceHolder;
 
+import net.kdt.pojavlaunch.MinecraftGLSurface.mSurface;
 import net.kdt.pojavlaunch.prefs.LauncherPreferences;
 import net.kdt.pojavlaunch.Tools;
+import org.lwjgl.glfw.CallbackBridge;
 
 public class ResolutionAdjuster {
 
@@ -86,9 +92,23 @@ public class ResolutionAdjuster {
     private void refreshSize() {
         int windowWidth = Tools.getDisplayFriendlyRes(Tools.currentDisplayMetrics.widthPixels, mScaleFactor);
         int windowHeight = Tools.getDisplayFriendlyRes(Tools.currentDisplayMetrics.heightPixels, mScaleFactor);
+        if (mSurface == null) {
+            Log.w("MGLSurface", "Attempt to refresh size on null surface");
+            return;
+        }
+        if (LauncherPreferences.PREF_USE_ALTERNATE_SURFACE) {
+            SurfaceView view = (SurfaceView) mSurface;
+            if (view.getHolder() != null) {
+                view.getHolder().setFixedSize(windowWidth, windowHeight);
+            }
+        } else {
+            TextureView view = (TextureView) mSurface;
+            if (view.getSurfaceTexture() != null) {
+                view.getSurfaceTexture().setDefaultBufferSize(windowWidth, windowHeight);
+            }
+        }
 
-        // 在此处添加更新分辨率的逻辑
-        // CallbackBridge.sendUpdateWindowSize(windowWidth, windowHeight);
+        CallbackBridge.sendUpdateWindowSize(windowWidth, windowHeight);
 
     }
 }
