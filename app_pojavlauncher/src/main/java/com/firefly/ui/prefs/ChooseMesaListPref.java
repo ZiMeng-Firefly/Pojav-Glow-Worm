849d1e2e2927203886db2a8e9d822b456728f004
Vera-Firefly <1964762970@qq.com>
2024-11-06 10:04:59 +0800

    测试

--- app_pojavlauncher/src/main/java/com/firefly/ui/prefs/ChooseMesaListPref.java
+++ app_pojavlauncher/src/main/java/com/firefly/ui/prefs/ChooseMesaListPref.java
@@ -6,6 +6,7 @@ import android.util.DisplayMetrics;
 import android.view.WindowManager;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
