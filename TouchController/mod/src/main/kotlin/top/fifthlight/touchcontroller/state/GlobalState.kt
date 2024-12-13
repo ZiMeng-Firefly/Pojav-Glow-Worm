e0d9568f7c7913b654a0304941b1ab3bb8da8971
Vera-Firefly <1964762970@qq.com>
2024-12-13 21:44:12 +0800

    测试

--- TouchController/mod/src/main/kotlin/top/fifthlight/touchcontroller/state/GlobalState.kt
+++
@@ -1,9 +0,0 @@
-package top.fifthlight.touchcontroller.state
-
-import top.fifthlight.touchcontroller.proxy.data.IntSize
-
-data class GlobalState(
-    val inGame: Boolean = false,
-    val windowSize: IntSize = IntSize.ZERO,
-    val scaleFactor: Float = 1f,
-)
