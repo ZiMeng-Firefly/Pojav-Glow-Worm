c4e0f2ff1aeabf8c1dfd282673f5e45e16a820b0
Vera-Firefly <1964762970@qq.com>
2024-12-18 14:37:36 +0800

    测试

--- app_pojavlauncher/src/main/jni/ctxbridges/osm_bridge_xxx3.c
+++ app_pojavlauncher/src/main/jni/ctxbridges/osm_bridge_xxx3.c
@@ -19,19 +19,6 @@
 
 static struct xxx3_osm_render_window_t *xxx3_osm;
 static bool hasCleaned = false;
-static bool hasSetNoRendererBuffer = false;
-static char xxx3_no_render_buffer[4];
-
-// void setNativeWindowSwapInterval(AHardwareBuffer* hardwareBuffer, int swapInterval);
-
-/*
-void xxx3_osm_set_no_render_buffer(AHardwareBuffer* buf) {
-    buf->bits = &xxx3_no_render_buffer;
-    buf->width = pojav_environ->savedWidth;
-    buf->height = pojav_environ->savedHeight;
-    buf->stride = 0;
-}
-*/
 
 void *xxx3OsmGetCurrentContext() {
     return (void *)OSMesaGetCurrentContext_p();
@@ -47,11 +34,6 @@ void xxx3_osm_apply_current_l() {
         printf("Zink: attempted to swap buffers without context!");
 
     OSMesaMakeCurrent_p(ctx, xxx3_osm->framebuffer, GL_UNSIGNED_BYTE, xxx3_osm->width, xxx3_osm->height);
-    /*
-    if (buf->stride != xxx3_osm->last_stride)
-        OSMesaPixelStore_p(OSMESA_ROW_LENGTH, buf->stride);
-    xxx3_osm->last_stride = buf->stride;
-    */
     OSMesaPixelStore_p(OSMESA_ROW_LENGTH, xxx3_osm->width * 4);
 }
 
@@ -61,12 +43,6 @@ void xxx3_osm_apply_current_ll() {
                                    GL_UNSIGNED_BYTE,
                                    xxx3_osm->width,
                                    xxx3_osm->height);
-
-    /*
-    if (buf->stride != xxx3_osm->last_stride)
-        OSMesaPixelStore_p(OSMESA_ROW_LENGTH, buf->stride);
-    xxx3_osm->last_stride = buf->stride;
-    */
     OSMesaPixelStore_p(OSMESA_ROW_LENGTH, xxx3_osm->width * 4);
 }
 
@@ -80,8 +56,7 @@ void xxx3OsmSwapBuffers() {
         &xxx3_osm->destBuffer);
     xxx3_osm_apply_current_l();
     glFinish_p();
-    int pixelSize = 4;
-    memcpy(xxx3_osm->destBuffer, xxx3_osm->framebuffer, xxx3_osm->width * xxx3_osm->height * pixelSize);
+    memcpy(xxx3_osm->destBuffer, xxx3_osm->framebuffer, xxx3_osm->width * xxx3_osm->height * 4);
     AHardwareBuffer_unlock(xxx3_osm->hardwareBuffer, NULL);
 }
 
@@ -91,7 +66,33 @@ void xxx3OsmMakeCurrent(void *window) {
         printf("OSMDroid: making current\n");
         // xxx3_osm->hardwareBuffer = pojav_environ->pojavWindow;
         AHardwareBuffer_acquire(xxx3_osm->hardwareBuffer);
-
+        AHardwareBuffer_Desc desc = {};
+        desc.width = xxx3_osm->width;
+        desc.height = xxx3_osm->height;
+        desc.format = AHARDWAREBUFFER_FORMAT_R8G8B8A8_UNORM;
+        desc.usage = AHARDWAREBUFFER_USAGE_CPU_READ_NEVER
+                     | AHARDWAREBUFFER_USAGE_CPU_WRITE_OFTEN
+                     | AHARDWAREBUFFER_USAGE_GPU_SAMPLED_IMAGE
+                     | AHARDWAREBUFFER_USAGE_GPU_COLOR_OUTPUT;
+        
+        xxx3_osm->desc = desc;
+        AHardwareBuffer_describe(xxx3_osm->hardwareBuffer, &xxx3_osm->desc);
+        int err = AHardwareBuffer_allocate(&xxx3_osm->desc, &xxx3_osm->hardwareBuffer);
+        if (err == 0)
+        {
+            // AHardwareBuffer_lock(xxx3_osm->hardwareBuffer, 0, &xxx3_osm->buffer);    
+            AHardwareBuffer_lock(
+                xxx3_osm->hardwareBuffer,
+                AHARDWAREBUFFER_USAGE_CPU_WRITE_OFTEN,
+                -1,
+                NULL,
+                &xxx3_osm->destBuffer);
+            memcpy(xxx3_osm->destBuffer, xxx3_osm->framebuffer, xxx3_osm->width * xxx3_osm->height * 4);
+            // AHardwareBuffer_unlock(xxx3_osm->hardwareBuffer);
+        } else {
+            printf("OSMDroid: allocate fail\n");
+        }
+        /*
         AHardwareBuffer_Desc desc =
         {
             xxx3_osm->width,
@@ -106,22 +107,14 @@ void xxx3OsmMakeCurrent(void *window) {
             0,
             0
         };
-        xxx3_osm->desc = desc;
-        AHardwareBuffer_describe(xxx3_osm->hardwareBuffer, &xxx3_osm->desc);
         AHardwareBuffer_allocate(&xxx3_osm->desc, &xxx3_osm->hardwareBuffer);
-
         AHardwareBuffer_lock(
             xxx3_osm->hardwareBuffer,
             AHARDWAREBUFFER_USAGE_CPU_WRITE_OFTEN,
             -1,
             NULL,
             &xxx3_osm->destBuffer);
-    }
-
-    if (!hasSetNoRendererBuffer)
-    {
-        hasSetNoRendererBuffer = true;
-        // xxx3_osm_set_no_render_buffer(&xxx3_osm->buffer);
+        */
     }
 
     xxx3_osm->window = window;
@@ -130,7 +123,7 @@ void xxx3OsmMakeCurrent(void *window) {
 
     if (!hasCleaned)
     {
-        hasCleaned = true;
+        // hasCleaned = true;
         printf("OSMDroid: vendor: %s\n", glGetString_p(GL_VENDOR));
         printf("OSMDroid: renderer: %s\n", glGetString_p(GL_RENDERER));
         glClear_p(GL_COLOR_BUFFER_BIT);
