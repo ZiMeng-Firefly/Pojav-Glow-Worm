6408e393baadd580167825f9e9047cf787f41d99
Vera-Firefly <1964762970@qq.com>
2024-12-20 23:59:08 +0800

    测试

--- app_pojavlauncher/src/main/jni/ctxbridges/osm_bridge_xxx2.c
+++ app_pojavlauncher/src/main/jni/ctxbridges/osm_bridge_xxx2.c
@@ -23,8 +23,6 @@ static bool swapSurface = false;
 static char xxx2_no_render_buffer[4];
 static const char* osm_LogTag = "[ XXX2 OSM Bridge ]";
 
-void* abuffer;
-
 void setNativeWindowSwapInterval(struct ANativeWindow* nativeWindow, int swapInterval);
 
 void xxx2_osm_set_no_render_buffer(ANativeWindow_Buffer* buf) {
@@ -72,7 +70,7 @@ void* xxx2OsmCreateContext(void* contextSrc) {
 
 
 void xxx2_osm_apply_current(ANativeWindow_Buffer* buf) {
-    if (swapSurface) xxx2_osm->context = OSMesaGetCurrentContext_p();
+    // if (swapSurface) xxx2_osm->context = OSMesaGetCurrentContext_p();
     OSMesaMakeCurrent_p(xxx2_osm->context, buf->bits, GL_UNSIGNED_BYTE, buf->width, buf->height);
     if (buf->stride != xxx2_osm->last_stride)
         OSMesaPixelStore_p(OSMESA_ROW_LENGTH, buf->stride);
@@ -96,9 +94,9 @@ void xxx2OsmMakeCurrent(void* window) {
     }
 
     
-    swapSurface = false;
+    // swapSurface = false;
     xxx2_osm->window = window;
-    xxx2_osm->context = xxx2_osm->window;
+    // xxx2_osm->context = xxx2_osm->window;
     xxx2_osm_apply_current(&xxx2_osm->buffer);
     OSMesaPixelStore_p(OSMESA_Y_UP, 0);
 
@@ -114,7 +112,7 @@ void xxx2OsmMakeCurrent(void* window) {
 }
 
 void xxx2OsmSwapBuffers() {
-    if (!swapSurface) swapSurface = true;
+    // if (!swapSurface) swapSurface = true;
     ANativeWindow_lock(xxx2_osm->nativeSurface, &xxx2_osm->buffer, NULL);
     xxx2_osm_apply_current(&xxx2_osm->buffer);
     glFinish_p();
