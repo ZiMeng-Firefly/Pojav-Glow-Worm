4a123248e0efaf2f178363373f1c10fa452466db
Vera-Firefly <1964762970@qq.com>
2024-12-20 22:17:10 +0800

    测试

--- app_pojavlauncher/src/main/jni/egl_bridge.c
+++ app_pojavlauncher/src/main/jni/egl_bridge.c
@@ -33,8 +33,6 @@
 #include "ctxbridges/osm_bridge.h"
 #include "ctxbridges/osm_bridge_xxx1.h"
 #include "ctxbridges/osm_bridge_xxx2.h"
-#include "ctxbridges/osm_bridge_xxx3.h"
-#include "ctxbridges/osm_bridge_xxx4.h"
 #include "ctxbridges/renderer_config.h"
 #include "ctxbridges/virgl_bridge.h"
 #include "driver_helper/nsbypass.h"
@@ -73,8 +71,6 @@ EXTERNAL_API void pojavTerminate() {
         case RENDERER_VK_ZINK:
         case RENDERER_VK_ZINK_XXX1:
         case RENDERER_VK_ZINK_XXX2:
-        case RENDERER_VK_ZINK_XXX3:
-        case RENDERER_VK_ZINK_XXX4:
             // Nothing to do here
             break;
     }
@@ -151,12 +147,6 @@ EXTERNAL_API void* pojavGetCurrentContext() {
     if (pojav_environ->config_renderer == RENDERER_VK_ZINK_XXX2)
         return xxx2OsmGetCurrentContext();
 
-    if (pojav_environ->config_renderer == RENDERER_VK_ZINK_XXX3)
-        return xxx3OsmGetCurrentContext();
-
-    if (pojav_environ->config_renderer == RENDERER_VK_ZINK_XXX4)
-        return xxx4OsmGetCurrentContext();
-
     return br_get_current();
 }
 
@@ -204,15 +194,12 @@ void renderer_load_config() {
             xxx2OsmInit();
             xxx2OsmloadSymbols();
         } break;
-        case BRIDGE_TBL_XXX3: {
-            pojav_environ->config_renderer = RENDERER_VK_ZINK_XXX3;
-            xxx3OsmInit();
-            xxx3OsmloadSymbols();
-        } break;
-        case BRIDGE_TBL_XXX4: {
-            pojav_environ->config_renderer = RENDERER_VK_ZINK_XXX4;
-            xxx4OsmloadSymbols();
-        } break;
+        case BRIDGE_TBL_XXX3:
+            // Nothing to do here
+            break;
+        case BRIDGE_TBL_XXX4:
+            // Nothing to do here
+            break;
         default: {
             pojav_environ->config_renderer = RENDERER_VK_ZINK;
             set_osm_bridge_tbl();
@@ -355,12 +342,6 @@ EXTERNAL_API void pojavSwapBuffers() {
     if (pojav_environ->config_renderer == RENDERER_VK_ZINK_XXX2)
         xxx2OsmSwapBuffers();
 
-    if (pojav_environ->config_renderer == RENDERER_VK_ZINK_XXX3)
-        xxx3OsmSwapBuffers();
-
-    if (pojav_environ->config_renderer == RENDERER_VK_ZINK_XXX4)
-        xxx4OsmSwapBuffers();
-
     if (pojav_environ->config_renderer == RENDERER_VK_ZINK_XXX1)
         br_swap_buffers();
 }
@@ -385,12 +366,6 @@ EXTERNAL_API void pojavMakeCurrent(void* window) {
     if (pojav_environ->config_renderer == RENDERER_VK_ZINK_XXX2)
         xxx2OsmMakeCurrent(window);
 
-    if (pojav_environ->config_renderer == RENDERER_VK_ZINK_XXX3)
-        xxx3OsmMakeCurrent(window);
-
-    if (pojav_environ->config_renderer == RENDERER_VK_ZINK_XXX4)
-        xxx4OsmMakeCurrent(window);
-
 }
 
 EXTERNAL_API void* pojavCreateContext(void* contextSrc) {
@@ -405,12 +380,6 @@ EXTERNAL_API void* pojavCreateContext(void* contextSrc) {
 
     if (pojav_environ->config_renderer == RENDERER_VK_ZINK_XXX2)
         return xxx2OsmCreateContext(contextSrc);
-
-    if (pojav_environ->config_renderer == RENDERER_VK_ZINK_XXX3)
-        return xxx3OsmCreateContext(contextSrc);
-
-    if (pojav_environ->config_renderer == RENDERER_VK_ZINK_XXX4)
-        return xxx4OsmCreateContext(contextSrc);
 
     return br_init_context((basic_render_window_t*)contextSrc);
 }
@@ -482,12 +451,6 @@ EXTERNAL_API void pojavSwapInterval(int interval) {
 
     if (pojav_environ->config_renderer == RENDERER_VK_ZINK_XXX2)
         xxx2OsmSwapInterval(interval);
-
-    if (pojav_environ->config_renderer == RENDERER_VK_ZINK_XXX3)
-        xxx3OsmSwapInterval(interval);
-
-    if (pojav_environ->config_renderer == RENDERER_VK_ZINK_XXX4)
-        xxx4OsmSwapInterval(interval);
 
     if (pojav_environ->config_renderer == RENDERER_VK_ZINK_XXX1)
     {
