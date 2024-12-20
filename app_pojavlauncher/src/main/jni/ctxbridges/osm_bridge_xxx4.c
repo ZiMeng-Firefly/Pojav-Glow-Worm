0fd3c40628daf1cc018f92f45de19b208987c047
Vera-Firefly <1964762970@qq.com>
2024-12-20 20:58:28 +0800

    测试

--- app_pojavlauncher/src/main/jni/ctxbridges/osm_bridge_xxx4.c
+++ app_pojavlauncher/src/main/jni/ctxbridges/osm_bridge_xxx4.c
@@ -17,6 +17,7 @@
 #include "renderer_config.h"
 
 static struct xxx4_osm_render_window_t *xxx4_osm;
+static bool swapBuffers = false;
 static bool hasCleaned = false;
 static bool hasSetNoRendererBuffer = false;
 static char xxx4_no_render_buffer[4];
@@ -45,36 +46,35 @@ void* xxx4OsmCreateContext(void* contextSrc) {
     memset(xxx4_osm, 0, sizeof(struct xxx4_osm_render_window_t));
 
     printf("OSMDroid: generating context\n");
-    OSMesaContext osmesa_share = NULL;
-    if (contextSrc != NULL) osmesa_share = contextSrc;
-    OSMesaContext context = OSMesaCreateContext_p(OSMESA_RGBA, osmesa_share);
+    void* context = NULL;
+    if (contextSrc != NULL)
+    {
+        context = OSMesaCreateContext_p(OSMESA_RGBA, contextSrc);
+    }
+
     if (context == NULL)
     {
         free(xxx4_osm);
         return NULL;
     }
-    xxx4_osm->context = context;
+
     printf("OSMDroid: context=%p\n", context);
     return context;
 }
 
-void xxx4_osm_apply_current_l(ANativeWindow_Buffer* buf) {
+void xxx4_osm_apply_current(ANativeWindow_Buffer* buf) {   
+    if (swapBuffers)
+        xxx4_osm->context = OSMesaGetCurrentContext_p();
     OSMesaMakeCurrent_p(xxx4_osm->context, buf->bits, GL_UNSIGNED_BYTE, buf->width, buf->height);
     if (buf->stride != xxx4_osm->last_stride)
         OSMesaPixelStore_p(OSMESA_ROW_LENGTH, buf->stride);
     xxx4_osm->last_stride = buf->stride;
 }
 
-void xxx4_osm_apply_current_ll(ANativeWindow_Buffer* buf) {
-    OSMesaMakeCurrent_p((OSMesaContext)xxx4_osm->window, setbuffer, GL_UNSIGNED_BYTE, buf->width, buf->height);
-    if (buf->stride != xxx4_osm->last_stride)
-        OSMesaPixelStore_p(OSMESA_ROW_LENGTH, buf->stride);
-    xxx4_osm->last_stride = buf->stride;
-}
-
 void xxx4OsmSwapBuffers() {
+    if (!swapBuffers) swapBuffers = true;
     ANativeWindow_lock(xxx4_osm->nativeSurface, &xxx4_osm->buffer, NULL);
-    xxx4_osm_apply_current_l(&xxx4_osm->buffer);
+    xxx4_osm_apply_current(&xxx4_osm->buffer);
     glFinish_p();
     ANativeWindow_unlockAndPost(xxx4_osm->nativeSurface);
 }
@@ -86,7 +86,6 @@ void xxx4OsmMakeCurrent(void* window) {
         xxx4_osm = NULL;
         return;
     }
-    xxx4_osm->window = window;
 
     if (!hasCleaned)
     {
@@ -103,16 +102,18 @@ void xxx4OsmMakeCurrent(void* window) {
         xxx4_osm_set_no_render_buffer(&xxx4_osm->buffer);
     }
 
+    swapBuffers = false;
+    xxx4_osm->context = (OSMesaContext)window;
     xxx4_osm_apply_current_ll(&xxx4_osm->buffer);
 
     if (!hasCleaned)
     {
         hasCleaned = true;
         OSMesaPixelStore_p(OSMESA_Y_UP, 0);
+        glClearColor_p(0.4f, 0.4f, 0.4f, 1.0f);
         printf("OSMDroid: vendor: %s\n", glGetString_p(GL_VENDOR));
         printf("OSMDroid: renderer: %s\n", glGetString_p(GL_RENDERER));
         glClear_p(GL_COLOR_BUFFER_BIT);
-        glClearColor_p(0.4f, 0.4f, 0.4f, 1.0f);
         ANativeWindow_unlockAndPost(xxx4_osm->nativeSurface);
     }
 }
