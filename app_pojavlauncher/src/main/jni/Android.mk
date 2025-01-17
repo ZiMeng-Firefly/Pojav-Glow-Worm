a8e81ee6d9295b35798806b84c0f84cb7a80b8cc
Vera-Firefly <1964762970@qq.com>
2025-01-17 22:04:41 +0800

    测试

--- app_pojavlauncher/src/main/jni/Android.mk
+++ app_pojavlauncher/src/main/jni/Android.mk
@@ -56,6 +56,25 @@ endif
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
+
+LOCAL_CPPFLAGS := -std=c++17
+LOCAL_LDLIBS := -llog -ldl -lc++
+LOCAL_MODULE := driver_helper
+LOCAL_SRC_FILES := driver_helper/nsbypass.c
+LOCAL_CFLAGS := -fPIC -g -rdynamic
+
+include $(BUILD_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := linkerhook
+LOCAL_SRC_FILES := driver_helper/hook.c
+LOCAL_LDFLAGS := -z global
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := pojavexec_awt
 LOCAL_SRC_FILES := \
     awt_bridge.c
