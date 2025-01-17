23776e3016d7ab6c9fb1554bfb6e85ae857322ef
Vera-Firefly <1964762970@qq.com>
2025-01-17 21:58:00 +0800

    测试

--- app_pojavlauncher/src/main/jni/Android.mk
+++ app_pojavlauncher/src/main/jni/Android.mk
@@ -47,22 +47,13 @@ LOCAL_SRC_FILES := \
     jre_launcher.c \
     utils.c \
     driver_helper.c\
-    stdio_is.c \
-    driver_helper/nsbypass.c
+    stdio_is.c
 
 ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
 LOCAL_CFLAGS += -DADRENO_POSSIBLE
 LOCAL_LDLIBS += -lEGL -lGLESv2
 endif
 include $(BUILD_SHARED_LIBRARY)
-
-#ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
-include $(CLEAR_VARS)
-LOCAL_MODULE := linkerhook
-LOCAL_SRC_FILES := driver_helper/hook.c
-LOCAL_LDFLAGS := -z global
-include $(BUILD_SHARED_LIBRARY)
-#endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := pojavexec_awt
--- app_pojavlauncher/src/main/jni/driver_helper/Android.mk
+++ app_pojavlauncher/src/main/jni/driver_helper/Android.mk
@@ -3,11 +3,18 @@ include $(CLEAR_VARS)
 LOCAL_CPPFLAGS := -std=c++17
 LOCAL_LDLIBS := -llog -ldl -lc++
 LOCAL_MODULE := driver_helper
-
 LOCAL_SRC_FILES := \
     driver_helper.c \
     nsbypass.c
-
 LOCAL_CFLAGS := -fPIC -g -rdynamic
+
+include $(BUILD_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := linkerhook
+LOCAL_SRC_FILES := hook.c
+LOCAL_LDFLAGS := -z global
 
 include $(BUILD_SHARED_LIBRARY)
