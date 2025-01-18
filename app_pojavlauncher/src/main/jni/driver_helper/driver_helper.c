2634a8fca65038f25c23e960a23cc5c35da9a616
Vera-Firefly <1964762970@qq.com>
2025-01-19 01:08:24 +0800

    测试

--- app_pojavlauncher/src/main/jni/driver_helper/driver_helper.c
+++ app_pojavlauncher/src/main/jni/driver_helper/driver_helper.c
@@ -101,10 +101,6 @@ void* loadTurnipVulkan() {
     linkerhookPassHandles(turnip_driver_handle, android_dlopen_ext, android_get_exported_namespace);
 
     void* libvulkan = linker_ns_dlopen_unique(cache_dir, "libvulkan.so", RTLD_LOCAL | RTLD_NOW);
-    if (!libvulkan) printf("libvulkan is null!\n");
-
-    dlclose(dl_android);
-    dlclose(linkerhook);
 
     return libvulkan;
 }
