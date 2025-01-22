2892a8dacb894f69d93d77be0a8a15ee9921b39a
Vera-Firefly <1964762970@qq.com>
2025-01-22 13:28:26 +0800

    测试

--- app_pojavlauncher/src/main/jni/driver_helper/hook.c
+++ app_pojavlauncher/src/main/jni/driver_helper/hook.c
@@ -1,62 +1,50 @@
 //
-// Created by Vera-Firefly on 17.01.2025.
+// Created by maks on 05.06.2023.
 //
-
 #include <android/dlext.h>
 #include <string.h>
 #include <stdio.h>
+static void* (*android_dlopen_ext_p)(const char* filename, int flags, const android_dlextinfo* extinfo, const void* caller_addr);
+static struct android_namespace_t* (*android_get_exported_namespace_p)(const char* name);
+static void* ready_handle;
 
-static void* (*android_dlopen_ext_impl)(const char* filename, int flags, const android_dlextinfo* extinfo, const void* caller_addr) = NULL;
-static struct android_namespace_t* (*android_get_exported_namespace_impl)(const char* name) = NULL;
-static void* ready_handle = NULL;
-
-static const char* const sphal_namespaces[] = {
-    "sphal", "vendor", "default"
+static const char *sphal_namespaces[3] = {
+        "sphal", "vendor", "default"
 };
 
-#define SPHAL_NAMESPACE_COUNT (sizeof(sphal_namespaces) / sizeof(sphal_namespaces[0]))
-
 __attribute__((visibility("default"), used))
-void linker_hook_set_handles(void* handle, void* android_dlopen_ext, void* android_get_exported_namespace) {
-    if (handle && android_dlopen_ext && android_get_exported_namespace) {
-        ready_handle = handle;
-        android_dlopen_ext_impl = (typeof(android_dlopen_ext_impl))android_dlopen_ext;
-        android_get_exported_namespace_impl = (typeof(android_get_exported_namespace_impl))android_get_exported_namespace;
-    }
+void linker_hook_pass_handles(void* data, void* android_dlopen_ext, void* android_get_exported_namespace) {
+    ready_handle = data;
+    android_dlopen_ext_p = android_dlopen_ext;
+    android_get_exported_namespace_p = android_get_exported_namespace;
 }
 
 __attribute__((visibility("default"), used))
-void* android_dlopen_ext(const char* filename, int flags, const android_dlextinfo* extinfo) {
+void *android_dlopen_ext(const char *filename, int flags, const android_dlextinfo *extinfo) {
     if (strstr(filename, "vulkan."))
         return ready_handle;
 
-    return android_dlopen_ext_impl(filename, flags, extinfo, &android_dlopen_ext);    
+    return android_dlopen_ext_p(filename, flags, extinfo, &android_dlopen_ext);
 }
 
-__attribute__((visibility("default"), used))
-void* android_load_sphal_library(const char* filename, int flags) {
-    if (strstr(filename, "vulkan."))
+__attribute__((visibility("default"), used)) void *android_load_sphal_library(const char *filename, int flags) {
+    if(strstr(filename, "vulkan."))
         return ready_handle;
 
-    struct android_namespace_t* androidNamespace = NULL;
-    for (size_t i = 0; i < SPHAL_NAMESPACE_COUNT && !androidNamespace; i++) {
-        androidNamespace = android_get_exported_namespace_impl(sphal_namespaces[i]);
-    }
-
-    if (!androidNamespace) {
-        fprintf(stderr, "Error: No valid SPHAL namespace found\n");
-        return NULL;
+    struct android_namespace_t* androidNamespace;
+    for(int i = 0; i < 3; i++) {
+        androidNamespace = android_get_exported_namespace_p(sphal_namespaces[i]);
+        if (androidNamespace != NULL) break;
     }
 
     android_dlextinfo info = {
         .flags = ANDROID_DLEXT_USE_NAMESPACE,
         .library_namespace = androidNamespace
-    };
+    }
 
-    return android_dlopen_ext_impl(filename, flags, &info, &android_dlopen_ext);
+    return android_dlopen_ext_p(filename, flags, &info, &android_dlopen_ext);
 }
 
-__attribute__((visibility("default"), used))
-uint64_t atrace_get_enabled_tags() {
+__attribute__((visibility("default"), used)) uint64_t atrace_get_enabled_tags() {
     return 0;
 }
