d78a07fb8e110e27c295c02785b0d425cbd9a7eb
Vera-Firefly <1964762970@qq.com>
2025-01-28 20:57:06 +0800

    测试

--- app_pojavlauncher/src/main/jni/Android.mk
+++ app_pojavlauncher/src/main/jni/Android.mk
@@ -14,7 +14,7 @@ LOCAL_PATH := $(HERE_PATH)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := angle_gles2
-LOCAL_SRC_FILES := angle/angle-gles/$(TARGET_ARCH_ABI)/libGLESv2_angle.so
+LOCAL_SRC_FILES := angle/angle-gles/$(TARGET_ARCH_ABI)/libGLES_angle.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 
