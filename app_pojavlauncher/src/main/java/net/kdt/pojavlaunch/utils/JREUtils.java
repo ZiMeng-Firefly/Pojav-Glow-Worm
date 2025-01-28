36baaf4f16965597b8eab9f68db8354286fbd078
Vera-Firefly <1964762970@qq.com>
2025-01-28 21:12:05 +0800

    测试

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
@@ -275,11 +275,6 @@ public class JREUtils {
             }
         }
 
-        if (LOCAL_RENDERER.equals("opengles3_angle")) {
-            envMap.put("LIBGL_ES", "3");
-            eglName = "libEGL_angle.so";
-        }
-
         if (eglName != null) envMap.put("POJAVEXEC_EGL", eglName);
 
         if (!LOCAL_RENDERER.startsWith("opengles")) {
@@ -738,9 +733,6 @@ public class JREUtils {
                     break;
                 case "panfrost":
                     renderLibrary = "libOSMesa_2300d.so";
-                    break;
-                case "opengles3_angle":
-                    renderLibrary = "libAngle.so";
                     break;
                 default:
                     Log.w("RENDER_LIBRARY", "No renderer selected, defaulting to opengles2");
--- app_pojavlauncher/src/main/jni/Android.mk
+++ app_pojavlauncher/src/main/jni/Android.mk
@@ -13,21 +13,6 @@ LOCAL_PATH := $(HERE_PATH)
 
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := angle_gles2
-LOCAL_SRC_FILES := angle/angle-gles/$(TARGET_ARCH_ABI)/libGLES_angle.so
-include $(PREBUILT_SHARED_LIBRARY)
-
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := Angle
-LOCAL_SHARED_LIBRARIES := angle_gles2
-LOCAL_SRC_FILES := angle/main.c angle/string_utils.c
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/angle
-LOCAL_CFLAGS += -g -rdynamic
-include $(BUILD_SHARED_LIBRARY)
-
-
-include $(CLEAR_VARS)
 LOCAL_LDLIBS := -ldl -llog -landroid
 LOCAL_MODULE := br_common
 LOCAL_SRC_FILES := \
