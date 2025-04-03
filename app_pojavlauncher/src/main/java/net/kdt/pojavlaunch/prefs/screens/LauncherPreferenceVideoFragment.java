c974d31501e3eea4d8b07525bc840afd08142d12
Vera-Firefly <1964762970@qq.com>
2025-04-04 02:46:34 +0800

    测试

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceVideoFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceVideoFragment.java
@@ -339,6 +339,7 @@ public class LauncherPreferenceVideoFragment extends LauncherPreferenceFragment 
         multidrawModeOptions.add(getString(R.string.mg_option_multidraw_mode_indirect));
         multidrawModeOptions.add(getString(R.string.mg_option_multidraw_mode_basevertex));
         multidrawModeOptions.add(getString(R.string.mg_option_multidraw_mode_multidraw_indirect));
+        multidrawModeOptions.add(getString(R.string.mg_option_multidraw_mode_drawelements));
         ArrayAdapter<String> multidrawModeAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner, multidrawModeOptions);
         enableCompatibleMode.setAdapter(multidrawModeAdapter);
         enableCompatibleMode.setSelection(Integer.parseInt(LauncherPreferences.MG_MULTIDRAWMODE_OPTION));
